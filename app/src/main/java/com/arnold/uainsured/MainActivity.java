1b783a56f1b4b70b81c75f2988b27084e16f9014
ma-1 <arnom71@gmail.com>
2020-08-09 09:26:40 +0300

    Included forgot password dialogbox

--- app/src/main/java/com/arnold/uainsured/MainActivity.java
+++ app/src/main/java/com/arnold/uainsured/MainActivity.java
@@ -1,14 +1,17 @@
 package com.arnold.uainsured;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
     TextView forgotPassword,registration;
     Button mLogin;
 
-
+    AlertDialog.Builder dialogBox;
+    AlertDialog alertDialog;
 
 
     @Override
@@ -32,14 +36,45 @@ public class MainActivity extends AppCompatActivity {
 
         mLogin = findViewById(R.id.login);
 
-        //Listener for loginButton
-        mLogin.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showDialogBox(R.layout.forgot_password_dialog);
             }
         });
 
 
     }
+
+    private void showDialogBox(int layout){
+
+        dialogBox = new AlertDialog.Builder(MainActivity.this);
+        View layoutView = getLayoutInflater().inflate(layout,null);
+
+        Button dialogButton = layoutView.findViewById(R.id.submit);
+        dialogBox.setView(layoutView);
+        alertDialog = dialogBox.create();
+
+        //TODO
+        //Circular Animation
+
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        alertDialog.show();
+
+
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+            }
+        });
+    }
 }
+
+
+
+
