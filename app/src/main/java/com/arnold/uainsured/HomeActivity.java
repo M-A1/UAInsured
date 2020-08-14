d229a8afbb98805943981c502fa5b6787cdaada6
ma-1 <arnom71@gmail.com>
2020-08-14 12:20:06 +0300

    HomeActivity with nav drawer

--- app/src/main/java/com/arnold/uainsured/HomeActivity.java
+++ app/src/main/java/com/arnold/uainsured/HomeActivity.java
@@ -1,14 +1,48 @@
 package com.arnold.uainsured;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class HomeActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ActionBarDrawerToggle drawerToggle;
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer);
+
+        setSupportActionBar(toolbar);
+
+        drawerToggle =  new ActionBarDrawerToggle(this, drawer,R.string.open_nav,R.string.close_nav);
+
+        drawerToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
     }
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+
 }
